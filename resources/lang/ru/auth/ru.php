7cbfbe567e39d2ecbc1639c5d306455e63065f56
v.snigerev <v.snigerev@gmail.com>
2019-11-21 13:40:24 +0300

    Перевод страницы верификации

---
+++ resources/lang/ru/auth/ru.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) 21.11.2019.
+ * File - ru.php
+ * Author - tor
+ */
+
+
+return [
+    'Verify_Your_Email_Address' => 'Подтвердите Ваш адрес электронной почты',
+    'Before proceeding' => 'Для продолжения, проверьте Ваше почту и перейдите по ссылке в письме.',
+    'If you did not receive the email' => 'Если Вы не получили письмо',
+    'click here to request another' => 'нажмите здесь, для повторной отправки ссылки активации',
+    'fresh verification link' => 'Новая ссылка была отправлена на Ваш адрес.',
+];
