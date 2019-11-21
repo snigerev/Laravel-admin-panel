17a659ed634f9ab68805e90759e4a61f96c177e7
v.snigerev <v.snigerev@gmail.com>
2019-11-21 17:53:56 +0300

    Перевод страницы верификации

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,21 +5,21 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card ">
-                    <div class="card-header text-center">{{ trans('auth/ru.Verify_Your_Email_Address')}}</div>
+                    <div class="card-header text-center">{{ trans('auth/verify.Verify_Your_Email_Address')}}</div>
 
                     <div class="card-body">
                         @if (session('resent'))
                             <div class="alert alert-success" role="alert">
-                                {{ trans('auth/ru.fresh verification link') }}
+                                {{ trans('auth/verify.fresh verification link') }}
                             </div>
                         @endif
 
-                        {{ trans('auth/ru.Before proceeding') }}
-                        {{ trans('auth/ru.If you did not receive the email') }},
+                        {{ trans('auth/verify.Before proceeding') }}
+                        {{ trans('auth/verify.If you did not receive the email') }},
                         <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                             @csrf
                             <button type="submit"
-                                    class="btn btn-link p-0 m-0 align-baseline">{{ trans('auth/ru.click here to request another') }}</button>
+                                    class="btn btn-link p-0 m-0 align-baseline">{{ trans('auth/verify.click here to request another') }}</button>
                             .
                         </form>
                     </div>
