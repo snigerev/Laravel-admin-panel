d4c067a384c6c82ae5fa952ad0be816032290bc4
v.snigerev <v.snigerev@gmail.com>
2019-11-27 09:26:19 +0300

    правка сборки

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 26.11.2019.
+ * Copyright (c) 27.11.2019.
  * File - webpack.mix.js
  * Author - tor
  */
 
 const mix = require('laravel-mix');
+const mix2 = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -16,13 +17,24 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.sass('resources/sass/mainPage/main.scss', 'public/css/mainPage')
-    .copyDirectory('resources/images', 'public/images');
+mix2.sass('resources/sass/mainPage/main.scss', 'public/css/mainPage')
+    .options({
+        processCssUrls: false
+    });
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/admin/admin.js', 'public/js/admin')
-    .sass('resources/sass/admin/admin.scss', 'public/css/admin')
-    .js('resources/js/admin/users.js', 'public/js/admin')
-    .sass('resources/sass/admin/users.scss', 'public/css/admin')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/admin/admin.js', 'public/js/admin');
+mix.sass('resources/sass/admin/admin.scss', 'public/css/admin')
+    .options({
+        processCssUrls: true
+    });
 
+mix.js('resources/js/admin/users.js', 'public/js/admin');
+mix.sass('resources/sass/admin/users.scss', 'public/css/admin');
+mix.sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: true
+    });
+
+
+mix.copyDirectory('resources/images', 'public/images');
