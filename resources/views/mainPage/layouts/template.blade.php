23921e7378d2d0bdd6c509df267b6924da963879
z1tka <kulazhenkov.v@gmail.com>
2019-11-28 17:15:08 +0300

    добавил еще один блок container в header, перенёс в него кнопки войти и регистрация. добавил в новый блок еще два эелемента -  кнопки заглушки. добавил еще два элемента в блок main. добавил новый блок footer.перенёс кратинку нижней панели(из .body) в footer.

--- resources/views/mainPage/layouts/template.blade.php
+++ resources/views/mainPage/layouts/template.blade.php
@@ -16,18 +16,43 @@
 <div id="app">
     <header>
         <div class="container">
+            <div class="row d-flex justify-content-center">
+                @guest
+                    <div class="">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="{{ asset('images/mainPage/logo.png') }}" alt="">
+                        </a>
+                    </div>
+                @else
+                    <div class="">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="{{ asset('images/mainPage/logo.png') }}" alt="">
+                        </a>
+                    </div>
+                @endguest
+            </div>
+        </div>
+
+        <div class="container">
             <div class="row d-flex">
                 @guest
                     <div class="col-3 top-button justify-content-center">
                         <a href="{{ route('register') }}" class="a-top">
                             <img class="top-left" src="{{ asset('images/mainPage/button_left.png') }}"
-                                                                             alt=""><span
+                                 alt=""><span
                                 class="text-top">{{ trans('mainPage/register.Register') }}</span></a>
                     </div>
-                    <div class="col-6 justify-content-center">
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            <img src="{{ asset('images/mainPage/logo.png') }}" alt="">
-                        </a>
+                    <div class="col-3 top-button justify-content-center">
+                        <a href="{{ route('register') }}" class="a-top">
+                            <img class="top-left" src="{{ asset('images/mainPage/button_left.png') }}"
+                                 alt=""><span
+                                class="text-top">{{ trans('mainPage/register.Register') }}</span></a>
+                    </div>
+                    <div class="col-3 top-button justify-content-center">
+                        <a href="{{ route('login') }}" class="a-top"><img class="top-left"
+                                                                          src="{{ asset('images/mainPage/button_right.png') }}"
+                                                                          alt=""><span
+                                class="text-top">{{ trans('mainPage/register.login') }}</span></a>
                     </div>
                     <div class="col-3 top-button justify-content-center">
                         <a href="{{ route('login') }}" class="a-top"><img class="top-left"
@@ -42,10 +67,21 @@
                                                        alt=""><span
                                 class="text-top">{{ $nick }}</span></a>
                     </div>
-                    <div class="col-6 justify-content-center">
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            <img src="{{ asset('images/mainPage/logo.png') }}" alt="">
-                        </a>
+                    <div class="col-3 top-button justify-content-center">
+                        <a href="#" class="a-top"><img class="top-left"
+                                                       src="{{ asset('images/mainPage/button_left.png') }}"
+                                                       alt=""><span
+                                class="text-top">{{ $nick }}</span></a>
+                    </div>
+                    <div class="col-3 top-button justify-content-center">
+                        <a href="#" class="a-top" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"><img class="top-left"
+                                                                                   src="{{ asset('images/mainPage/button_right.png') }}"
+                                                                                   alt=""><span
+                                class="text-top">{{ trans('mainPage/register.logout') }}</span></a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+                            @csrf
+                        </form>
                     </div>
                     <div class="col-3 top-button justify-content-center">
                         <a href="#" class="a-top" onclick="event.preventDefault();
@@ -73,17 +109,25 @@
     </section>
     <main class="py-4 d-flex">
         <div class="col-3 left-main">
-            <img src="{{ asset('images/mainPage/left_panel2.png') }}" alt="">
+            @yield('content')
         </div>
         <div class="col-6 d-flex justify-content-center">
             @yield('content')
         </div>
         <div class="col-3 right-main">
-            <img src="{{ asset('images/mainPage/right_panel2.png') }}" alt="">
+            @yield('content')
         </div>
     </main>
 </div>
-@yield('footer')
+<footer>
+    <div class="container">
+        <div class="">
+            <a class="navbar-footer" href="{{ url('/') }}">
+                <img src="{{ asset('images/mainPage/panel_down2.png') }}" alt="">
+            </a>
+        </div>
+    </div>
+</footer>
 <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
